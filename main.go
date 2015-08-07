30c20c993fcf3a752c19b6ca95ac19c2999b2cd3
Ryosuke Akiyama <ryosuke.akiyama@broadleaf.co.jp>
2015-08-07 10:42:50 +0900

    Gin によるサーバーを追加

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
-func main() {
+func _main() (int, error) {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
 
 	r.Run(":8080")
+
+	return 0, nil
+}
+
+func main() {
+	if status, err := _main(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(status)
+	}
 }
