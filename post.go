3ddee2e60b5f8a8a274140e544a69f51757f33ed
Ryosuke Akiyama <ryosuke.akiyama@broadleaf.co.jp>
2015-08-07 15:19:51 +0900

    post 出たの登録・取得をDBから行うよう変更

--- post.go
+++ post.go
@@ -15,17 +15,9 @@ func GetPosts(c *gin.Context) {
 }
 
 func getPosts(c *gin.Context) *Error {
-	posts := make([]model.Post, 0, 10)
-
-	for i := 0; i < 10; i++ {
-		post := model.Post{
-			Model:    model.Model{Id: model.ID(i + 1)},
-			UserId:   model.ID(1),
-			Message:  "こんにちは！",
-			Url:      "https://www.mijs.jp/wp-content/themes/mijs-site/images/common/h1-logo.jpg",
-			UserName: "mijs",
-		}
-		posts = append(posts, post)
+	posts, err := database.ReadPosts(20)
+	if err != nil {
+		return NewError(http.StatusInternalServerError, "Can not read posts.", &err)
 	}
 
 	c.JSON(http.StatusOK, posts)
@@ -47,10 +39,10 @@ func newPost(c *gin.Context) *Error {
 		return NewError(http.StatusBadRequest, "Bad Request", &err)
 	}
 
-	//	err = database.WriteUser(&user)
-	//	if err != nil {
-	//		return NewError(http.StatusInternalServerError, "User can not be written.", &err)
-	//	}
+	err = database.WritePost(&post)
+	if err != nil {
+		return NewError(http.StatusInternalServerError, "Post can not be written.", &err)
+	}
 
 	c.JSON(http.StatusCreated, post.Id)
 
